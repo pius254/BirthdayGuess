87d0803b16fc239c150e15d2f1ab7634bb1a7a1d
Pius Wanjiru <piusmuriithi1@gmail.com>
2017-01-31 15:08:01 +0300

    use a multi-dimensional array to hold the dates

--- src/guessbirthday/GuessBirthday.java
+++ src/guessbirthday/GuessBirthday.java
@@ -5,8 +5,8 @@
  */
 package guessbirthday;
 
-//import java.util.Scanner;
-import javax.swing.JOptionPane;
+import java.util.Scanner;
+//import javax.swing.JOptionPane;
 
 /**
  *
@@ -18,69 +18,57 @@ public class GuessBirthday {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String set1 =
-               " 1 3 5 7\n" +
-               " 9 11 13 15\n" +
-               "17 19 21 23\n" +
-               "25 27 29 31";
+        int day = 0;//Day to be determined
+        int answer;
         
-        String set2 =
-               " 2 3 6 7\n" +
-               "10 11 14 15\n" +
-               "18 19 22 23\n" +
-               "26 27 30 31";
-        
-         String set3 =
-                 " 4 5 6 7\n" +
-                 "12 13 14 15\n" +
-                 "20 21 22 23\n" +
-                 "28 29 30 31";
-
-        String set4 =
-                " 8 9 10 11\n" +
-                "12 13 14 15\n" +
-                "24 25 26 27\n" +
-                "28 29 30 31";
-        
-        String set5 =
-                "16 17 18 19\n" +
-                "20 21 22 23\n" +
-                "24 25 26 27\n" +
-                "28 29 30 31";
-        
-       int day = 0;
+        int[][][] dates = {
+            {{ 1, 3, 5, 7},
+              { 9, 11, 13, 15},
+              { 17, 19, 21, 23},
+              { 25, 27, 29, 31}
+            },
+            {{ 2, 3, 6, 7},
+              { 10, 11, 14, 15},
+              { 18, 19, 22, 23},
+              { 26, 27, 30, 31}
+            },
+            {{ 4, 5, 6, 7},
+              { 12, 13, 14, 15},
+              { 20, 21, 22, 23},
+              { 28, 29, 30, 31}
+            },
+            {{ 8, 9, 10, 11},
+              { 12, 13, 14, 15},
+              { 24, 25, 26, 27},
+              { 28, 29, 30, 31}
+            },
+            {{ 16, 17, 18, 19},
+              { 20, 21, 22, 23},
+              { 24, 25, 26, 27},
+              { 28, 29, 30, 31}
+            }
+        };
        
-       //Create the Scanner
-       //Scanner input = new Scanner(System.in);
-       
-       //Prompt user to answer questions
-        int answer = JOptionPane.showConfirmDialog(null, "Is your Birthday in these numbers?\n" + set1);
+        Scanner input = new Scanner(System.in);
         
-        if(answer == JOptionPane.YES_OPTION)
-            day += 1;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Is your Birthday in Set " + (i + 1)+ "?");
+//            answer = JOptionPane.showConfirmDialog(null, "Is your Birthday in Set" + (i + 1)+ "?");
+            for (int j = 0; j < 4; j++) {
+                for(int k = 0; k < 4; k++)
+                    System.out.printf("%4d", dates[i][j][k]);
+                System.out.println();
+//                    JOptionPane.showMessageDialog(null, dates[i][j][k]);
+            }
+            System.out.println("Enter 0 for NO and 1 for YES");
+            answer = input.nextInt();
+            if (answer == 1) {
+                day += dates[i][0][0];
+            } 
+        }
         
-        answer = JOptionPane.showConfirmDialog(null, "Is your Birthday in these numbers?\n" + set2);
-        
-        if(answer == JOptionPane.YES_OPTION)
-            day += 2;
-        
-        answer = JOptionPane.showConfirmDialog(null, "Is your Birthday in these numbers?\n" + set3);
-        
-        if(answer == JOptionPane.YES_OPTION)
-            day += 4;
-        
-        answer = JOptionPane.showConfirmDialog(null, "Is your Birthday in these numbers?\n" + set4);
-        
-        if(answer == JOptionPane.YES_OPTION)
-            day += 8;
-        
-        answer = JOptionPane.showConfirmDialog(null, "Is your Birthday in these numbers?\n" + set5);
-        
-        if(answer == JOptionPane.YES_OPTION)
-            day += 16;
-        
-        JOptionPane.showMessageDialog(null, "\nYour Birthday is " + day + " ! ", "BIRTHDAYGUESS", JOptionPane.INFORMATION_MESSAGE);
-        
-    }
+        System.out.println("Your Birthday is "+ day);
+//        JOptionPane.showMessageDialog(null, "Your Birthday is " + day);
+    }   
     
 }
